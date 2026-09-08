ad16173fc4c76260fcaecf1815f895d54a119287
TYESSENBEKOV <talgat.yessenbekov@dnapayments.com>
2026-09-08 07:15:51 +0000

    Force landscape orientation in PWA
    Lock via screen.orientation.lock() on Android, plus portrait
    overlay prompting rotation on iOS and other devices.

--- src/main.jsx
+++ src/main.jsx
@@ -3,4 +3,8 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/app.css";
 
+if (screen.orientation?.lock) {
+  screen.orientation.lock("landscape-primary").catch(() => {});
+}
+
 createRoot(document.getElementById("root")).render(<App />);
