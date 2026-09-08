3b1df2c12662bf2639cacb5fb01bcbf17bcda96c
TYESSENBEKOV <talgat.yessenbekov@dnapayments.com>
2026-09-08 07:28:20 +0000

    Revert screen rotation lock and portrait overlay

--- src/main.jsx
+++ src/main.jsx
@@ -3,8 +3,4 @@ import { createRoot } from "react-dom/client";
 import App from "./App.jsx";
 import "./styles/app.css";
 
-if (screen.orientation?.lock) {
-  screen.orientation.lock("landscape-primary").catch(() => {});
-}
-
 createRoot(document.getElementById("root")).render(<App />);
